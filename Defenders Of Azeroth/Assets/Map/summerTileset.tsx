af949b919a1911b8bbac9340a31c7b0d75e3fea9
Constantin Mihalache <mihalache.c94@gmail.com>
2017-10-24 18:42:07 +0300

    Map draft. For now in Tiled format only.

---
+++ Defenders Of Azeroth/Assets/Map/summerTileset.tsx
@@ -0,0 +1,297 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="summer" tilewidth="31" tileheight="31" spacing="2" margin="1" tilecount="380" columns="19">
+ <image source="../Sprites/Map/summer.png" width="626" height="659"/>
+ <tile id="206">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="16.75">
+    <polyline points="0,0 18,-16.5 0.5,-16.75 0.25,-0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="207">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.5" y="18">
+    <polyline points="0,0 18.25,-18 0.5,-18 0.5,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="208">
+  <objectgroup draworder="index">
+   <object id="1" x="16.25" y="0.25">
+    <polyline points="0,0 3.5,5.5 4,12.75 10.5,14.5 14.5,18.5 14.75,-0.25 -0.25,-0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="209">
+  <objectgroup draworder="index">
+   <object id="2" x="13.25" y="-0.25">
+    <polyline points="0,0 17.25,18 17.75,0.25 0.25,0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="210">
+  <objectgroup draworder="index">
+   <object id="1" x="0.5" y="18">
+    <polyline points="0,0 8,-6.75 12.5,-6.5 20.75,-1.75 30.25,-0.5 30.5,-18 -0.5,-18 -0.75,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="211">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="18.25">
+    <polyline points="0,0 8.75,1.75 18.25,-0.25 25.25,-1 31,-1.5 31.25,-18.25 0.25,-18.25 0,-0.75"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="212">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="16.75">
+    <polyline points="0,0 9.5,-2.25 19,-1.5 31,1 31,-16.75 0,-16.75 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="214">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="12.25">
+    <polyline points="0,0 19,18.75 0,18.5 -0.5,-0.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="215">
+  <objectgroup draworder="index">
+   <object id="1" x="17.5" y="30">
+    <polyline points="0,0 -6.25,-19.25 1.75,-29.75 -17.5,-30 -17.5,1 -0.75,0.75"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="216">
+  <objectgroup draworder="index">
+   <object id="1" x="15.75" y="30.5">
+    <polyline points="0,0 3.75,-16.25 3.75,-30.5 -15.75,-30.5 -15.75,0.5 0.25,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="217">
+  <objectgroup draworder="index">
+   <object id="1" x="16" y="30.25">
+    <polyline points="0,0 -0.75,-12.25 -0.25,-21.5 1.75,-30 -16,-30.25 -15.75,0.25 -0.5,0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="218">
+  <objectgroup draworder="index">
+   <object id="1" x="16.5" y="30.5">
+    <polyline points="0,0 14.25,-10.75 14.5,-30.5 -16.5,-30.5 -16.5,0.5 -0.25,0.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="219">
+  <objectgroup draworder="index">
+   <object id="1" x="17.25" y="30.25">
+    <polyline points="0,0 4.25,-7.25 3.75,-15.5 5.75,-15.75 13.25,-14.25 13.75,0.75 -0.5,0.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="220">
+  <objectgroup draworder="index">
+   <object id="1" x="17.5" y="31">
+    <polyline points="0,0 13.25,-11.5 13.5,0 0.25,-0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="221">
+  <objectgroup draworder="index">
+   <object id="1" x="11" y="29.5">
+    <polyline points="0,0 19.25,-16.5 20,1.5 0,1 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="222">
+  <objectgroup draworder="index">
+   <object id="1" x="11.25" y="30">
+    <polyline points="0,0 2.75,-5.5 1,-11.5 15.5,-12.5 19.75,-18 19.75,1 -0.25,0.75"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="223">
+  <objectgroup draworder="index">
+   <object id="1" x="12.5" y="30">
+    <polyline points="0,0 -2.5,-3.25 -4,-15.75 -12.75,-13.5 -12.75,-30 6.5,-30.5 18.25,-19.25 18.5,0.25 0.5,0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="224">
+  <objectgroup draworder="index">
+   <object id="1" x="12.25" y="30.25">
+    <polyline points="0,0 7.5,-19.25 -1.25,-30.5 18.5,-30.25 18.75,0.25 0.25,0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="225">
+  <objectgroup draworder="index">
+   <object id="1" x="13.5" y="28.75">
+    <polyline points="0,0 -1.75,-12.5 0.25,-28.75 17,-28.75 17.5,2 -1,2.25 0.25,-1"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="300">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="301">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="-0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="302">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="303">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="-0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="304">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="305">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="306">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="307">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="308">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="309">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="310">
+  <objectgroup draworder="index">
+   <object id="1" x="0.5" y="0" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="311">
+  <objectgroup draworder="index">
+   <object id="1" x="0.5" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="312">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="313">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="314">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="315">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="316">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="317">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="-0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="318">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="319">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="320">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="321">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="322">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="323">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="-0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="324">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="325">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="326">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="327">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="0" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="328">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="329">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="-0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="330">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="0.25" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="331">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0" width="31" height="31"/>
+  </objectgroup>
+ </tile>
+</tileset>
