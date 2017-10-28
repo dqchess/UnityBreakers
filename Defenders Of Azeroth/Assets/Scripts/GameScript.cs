bbb598d80f7d8cbb8962536807ec9cf16e0d122e
Constantin Mihalache <mihalache.c94@gmail.com>
2017-10-28 18:24:50 +0300

    Add tower, base, healthbars and firing bullets.

---
+++ Defenders Of Azeroth/Assets/Scripts/GameScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+    public GameObject towerPrefab;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed left click.");
+            //Vector3 offset = new Vector3(120, -50, 0);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 towerPos = new Vector3(mousePos.x, mousePos.y, 0);
+            GameObject tower = (GameObject)Instantiate(towerPrefab, towerPos, Quaternion.identity);
+        }
+    }
+}
---
+++ Defenders Of Azeroth/Assets/Scripts/TowerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerScript : MonoBehaviour {
+    public Rigidbody2D bulletRigidbody;
+    public GameObject healthBar;
+
+	// Use this for initialization
+	void Start () {
+        healthBar.transform.SetParent(transform, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            // TODO add rotation depending on targeted enemy
+            Rigidbody2D bullet = Instantiate(bulletRigidbody, transform.position, Quaternion.identity) as Rigidbody2D;
+            bullet.velocity = new Vector3(100, 0, 0);
+
+            Vector3 cursorInWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 direction = cursorInWorldPos - transform.position;
+            direction.Normalize();
+            bullet.velocity = direction * 500;
+        }
+    }
+}
