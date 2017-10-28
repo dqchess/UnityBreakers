0f1285c802069c807746b07c75412a90279626c2
Constantin Mihalache <cmihalache@bitdefender.com>
2017-10-28 22:56:15 +0300

    Crazy merge

---
+++ Defenders Of Azeroth/Assets/Scripts/Defender_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defender_Controller : MonoBehaviour {
+
+    public static int movespeed = 10;
+    public Vector3 userDirection = Vector3.left;
+    private Animator anim;
+
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+        transform.Rotate(0, 180, 0);
+    }
+    public void Update()
+    {
+       
+        transform.Translate(userDirection * movespeed * Time.deltaTime, Space.Self);
+        anim.SetFloat("MoveX", -2);
+    }
+}
---
+++ Defenders Of Azeroth/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public int movespeed = 1;
+    public Vector3 userDirection = Vector3.right;
+    private Animator anim;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        transform.Translate(userDirection * movespeed * Time.deltaTime);
+
+        anim.SetFloat("MoveX", 1);
+    }
+}
