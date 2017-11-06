c4d09f21174789538dee0b8b1322471553ab1bb1
Constantin Mihalache <cmihalache@bitdefender.com>
2017-11-06 16:59:28 +0200

    Wave system

--- Defenders Of Azeroth/Assets/Scripts/GameScript.cs
+++ Defenders Of Azeroth/Assets/Scripts/GameScript.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class GameScript : MonoBehaviour {
     public GameObject towerPrefab;
 
-    public UnitsButton ClickedBtn { get; private set; } 
+    public UnitsButton ClickedBtn { get; private set; }
 
 
+    public GameObject enemy1Prefab;
+    public Transform enemySpawnPointUp;
+    public Transform enemySpawnPointDown;
+    public Transform enemyTarget;
+
+    float elapsedTime = 0f;
+    float targetTime = 5f;
+
+    // wave system relate
+    
 	// Use this for initialization
 	void Start () {
     }
@@ -23,7 +33,29 @@ public class GameScript : MonoBehaviour {
         //    gameobject tower = (gameobject)instantiate(towerprefab, towerpos, quaternion.identity);
         //}
 
+        // wave system
+        {
+            elapsedTime += Time.deltaTime;
+
+            if (elapsedTime >= targetTime)
+            {
+                elapsedTime = 0;
+
+                int numEnemiesToSpawn = 1 + (int)(Random.value * 100) % 2;
+                bool spawnUp = ( (int)(Random.value * 100) % 2) == 1;
+
+                Transform spawnLocation = (spawnUp) ? enemySpawnPointUp : enemySpawnPointDown;
+
+                for (int i = 0; i < numEnemiesToSpawn; i++)
+                {
+                    GameObject enemy = Instantiate(enemy1Prefab, spawnLocation.position - new Vector3(30 * i, 0, 0), enemy1Prefab.GetComponent<Transform>().rotation);
+                    enemy.GetComponent<AIPath>().target = enemyTarget;
+                    enemy.GetComponent<AIPath>().SearchPath();
+                }
+            }
+        }
     }
+
 
 
 }
